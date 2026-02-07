bf5aaf795dc8852b3a4b98d671bd529542089264
seif-el-sayed1 <seifsayed121@gmail.com>
2026-02-07 02:45:04 +0200

    add  actions , chats  routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,17 @@
 const appRouter = require("express").Router();
 const BASE_URL = "/api/v1";
-
 const ApiError = require("../utils/ApiError");
+
 let userAuthRoutes = require("./userAuth.routes")
+let chatRoutes = require("./chat.routes")
+let actionsRoutes = require("./actions.routes")
 
 const { app } = require("firebase-admin");
 
 
 appRouter.use(`${BASE_URL}/users/auth`, userAuthRoutes);
+appRouter.use(`${BASE_URL}/chats`, chatRoutes);
+appRouter.use(`${BASE_URL}/actions`, actionsRoutes);
 
 appRouter.get("/", (req, res) => {
   res.status(200).json({
